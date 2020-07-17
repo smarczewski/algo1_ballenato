829e0bad1bbd1f7a8e15dcf1d891262fad867bda
Gaston520 <65978435+Gaston520@users.noreply.github.com>
2020-07-17 16:56:19 -0300

    Cambios a programa_principal.py
    Elimine el archivo v2 y los cambios lo agregue al original.
    Agregue una funcion que regresa al menu para que la funcion pricipal no este tan cargada.

--- programa_principal.py
+++ programa_principal.py
@@ -18,36 +18,68 @@ import funcionalidad_5
 import func_1_v2
 
 CARPETA_FUNCIONES_ORDENADAS = "funciones"
-menu = ["1. Panel general de funciones",
+
+
+def mostrar_menu():
+    menu = ["Menu de opciones:",
+        "1. Panel general de funciones",
         "2. Consulta de funciones",
         "3. Analizador de reutilizacion de codigo",
         "4. Arbol de invocacion",
         "5. Informacion por desarrollador"]
 
-def mostrar_menu():
-    print(ascii_arts.titulo)
-    print(ascii_arts.ballena)
-    input("Presione cualquier tecla para continuar... ")
     for linea in menu:
         print(linea)
-    opcion = input("Opcion: ")
+    opcion = input("Opcion: ")    
 
     return opcion
+
+def regreso_al_menu():
+    """[Autor: Gaston Proz]
+    [Ayuda: Funcion que regresa al menu anterior]
+    """    
+    eleccion = input("\n\n¿Desea volver al menu anterior? si/no:\n ")
+    continuar = True
+    valido = False
+    while not valido:
+        if eleccion.lower().lstrip() == "no":
+            continuar = False
+            valido = True
+        elif eleccion.lower().lstrip() == "si":
+            print("\n"*100)            
+            valido = True
+        else:
+            eleccion = input("Respuesta invalida, intente otra vez:\n")
+    return continuar
 
 def funcion_principal():
     programas = open("programas.txt")
     ordenar.generar_arch_ordenados(programas)
     generar_archivos_csv.generar_csv()
     programas.close()
-    opcion = mostrar_menu()
-    if opcion == "1":
-        func_1_v2.princ()
-    if opcion == "3":
-        funcionalidad_3.imprimir_tabla_inv()
-    elif opcion == "4":
-        funcionalidad_4.generar_arbol()
-    elif opcion == "5":
-        funcionalidad_5.funcionalidad()
+    print(ascii_arts.titulo)
+    print(ascii_arts.ballena)
+    input("Presione Enter para continuar... ")
+    print("\n"*100)    
+    continuar = True
+    while continuar:
+        opcion = mostrar_menu()    
+        if opcion == "1":
+            func_1_v2.princ()
+            
+        elif opcion == "3":
+            funcionalidad_3.imprimir_tabla_inv()           
+
+        elif opcion == "4":
+            funcionalidad_4.generar_arbol()       
+
+        elif opcion == "5":
+            funcionalidad_5.funcionalidad()
+        
+        continuar = regreso_al_menu()
+                
+
+           
 
 #-------Invocacion de la funcion principal-------#
 funcion_principal()
