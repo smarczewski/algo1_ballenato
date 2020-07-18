1fd481e8d06c352d2f185eed9bc89590a2339be7
smarczewski <smarczewski@fi.uba.ar>
2020-07-18 19:51:46 -0300

    Cambio de nombre a obtener_lista_funciones para poder arreglar un bug en la funcionalidad_3.

--- funcionalidad_2.py
+++ funcionalidad_2.py
@@ -6,7 +6,7 @@ def leer_lineas_csv(archivo):
     return linea
 
 
-def obtener_lista_funciones(archivo):
+def obtener_lista_funciones2(archivo):
     """[Autor: Santiago Marczewski]
     [Ayuda: Obtiene una lista de nombres de las funciones en la aplicacion]"""
     archivo.seek(0)
@@ -43,7 +43,7 @@ def mostrar_tabla(fuente_unico, comentarios):
     [Ayuda: Imprime por pantalla una tabla de las funciones de la aplicacion
     formateado de manera similar a la tabla de built-in functions de la
     documentacion de Python]"""
-    funciones = obtener_lista_funciones(fuente_unico)
+    funciones = obtener_lista_funciones2(fuente_unico)
     largo = obtener_nombre_mas_largo(funciones)
     lista_cortada = cortar_lista_funciones(funciones, largo)
     for funcion in lista_cortada:
@@ -190,7 +190,7 @@ def mostrar_todo(tipo, imprimir, fuente_unico, comentarios):
     """[Autor: Santiago Marczewski]
     [Ayuda: Muestra por pantalla la informacion correspondiente (? o #) para todas las
     funciones de la aplicacion]"""
-    funciones = obtener_lista_funciones(fuente_unico)
+    funciones = obtener_lista_funciones2(fuente_unico)
     if imprimir:
         texto = open("ayuda_funciones.txt", "w")
         print("Información asociada a las funciones de la aplicación: \n", file=texto)
@@ -227,7 +227,7 @@ def validar_funcion(funcion, fuente_unico):
     """[Autor: Santiago Marczewski]
     [Ayuda: Verifica que la funcion sea valida]"""
     valida = True
-    funciones = obtener_lista_funciones(fuente_unico)
+    funciones = obtener_lista_funciones2(fuente_unico)
     funciones.append("todo")
     nombre, tipo, imprimir = procesar_pedido(funcion)
     if nombre not in funciones:
